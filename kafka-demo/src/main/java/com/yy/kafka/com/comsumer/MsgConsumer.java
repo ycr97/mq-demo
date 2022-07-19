cfe178c8086c5f3e7edea17a677e5da7d834a7fd
ycr <1396536724@qq.com>
2022-07-19 22:37:03 +0800

    【消费】修改写入文件

--- kafka-demo/src/main/java/com/yy/kafka/com/comsumer/MsgConsumer.java
+++ kafka-demo/src/main/java/com/yy/kafka/com/comsumer/MsgConsumer.java
@@ -4,6 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * @author ycr
  * @date 2022/7/19 22:30
@@ -14,6 +17,10 @@ public class MsgConsumer {
     @KafkaListener(topics = {"test_topic"}, groupId = "test_group")
     public void msgConsumer(ConsumerRecord<String, String> record) {
         record.value();
-        
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("D:/mq-test.txt"))) {
+            writer.write(record.value());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

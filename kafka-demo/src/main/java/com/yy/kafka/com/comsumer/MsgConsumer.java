f7d4bb29d5c4ef92528321f48fbd845c5cd10956
ycr <1396536724@qq.com>
2022-07-19 22:37:51 +0800

    【消费】修改写入文件

--- kafka-demo/src/main/java/com/yy/kafka/com/comsumer/MsgConsumer.java
+++ kafka-demo/src/main/java/com/yy/kafka/com/comsumer/MsgConsumer.java
@@ -4,8 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author ycr
@@ -17,7 +18,7 @@ public class MsgConsumer {
     @KafkaListener(topics = {"test_topic"}, groupId = "test_group")
     public void msgConsumer(ConsumerRecord<String, String> record) {
         record.value();
-        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("D:/mq-test.txt"))) {
+        try (OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(Paths.get("D:/mq-test.txt")))) {
             writer.write(record.value());
         } catch (Exception e) {
             throw new RuntimeException(e);
